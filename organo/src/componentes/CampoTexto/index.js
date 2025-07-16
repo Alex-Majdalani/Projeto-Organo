4b722d49350f2adf73f9951aca406c1bcef15e09
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-07-16 18:32:52 -0300

    campo texto

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -1,10 +1,22 @@
 import "./CampoTexto.css";
 
+let valor = "";
+
+const aoDigitado = (evento) => {
+  valor = evento.target.value;
+  console.log(valor);
+};
+
 const CampoTexto = (props) => {
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input required={props.obrigatorio} placeholder={props.placeholder} />
+      <input
+        value={valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={props.placeholder}
+      />
     </div>
   );
 };
