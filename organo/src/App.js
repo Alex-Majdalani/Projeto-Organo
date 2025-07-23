4861cbabbd5792eb7fb888fe972e202393d60b1b
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-07-23 14:36:31 -0300

    adicionei estado para colaboradores, dessa forma estou guardando as informações do formulario num array

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
 
 function App() {
+  /*Criando estados para os colaboradores */
+  const [colaboradores, setcolaboradores] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+    setcolaboradores([...colaboradores, colaborador]);
+  };
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorAdicionado(colaborador)
+        }
+      />
     </div>
   );
 }
