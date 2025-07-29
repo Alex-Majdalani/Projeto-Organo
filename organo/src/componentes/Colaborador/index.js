e2e6c966a2d0bcd00f515b824f0de47553894d1d
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-07-29 14:00:26 -0300

    Criei o componente Colaborador, onde foi criado o card para cada colaborador em htm e css

---
+++ organo/src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img src="https://github.com/Alex-Majdalani.png" alt="Alex Majdalani" />
+      </div>
+      <div className="rodape">
+        <h4>Alex Majdalani</h4>
+        <h5>Engenheiro de Softwares</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
