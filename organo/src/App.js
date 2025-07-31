859fb43d98b29c99f72dd7b1c122362e708de107
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-07-31 15:01:03 -0300

    Corrigi erro de chave key e adicionei a corPrimaria ao card do colaborador

--- organo/src/App.js
+++ organo/src/App.js
@@ -47,7 +47,7 @@ function App() {
   const [colaboradores, setcolaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador);
+    //console.log(colaborador);
     setcolaboradores([...colaboradores, colaborador]);
   };
 
